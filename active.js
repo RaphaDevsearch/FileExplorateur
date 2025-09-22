e102366e3b7cf174700858f9fec3c43363da15ae
RaphaDevsearch <mizaraphaelpro@gmail.com>
2025-09-22 10:16:54 +0530

    Refactor link activation logic to use indexed loop for improved clarity

--- active.js
+++ active.js
@@ -1,8 +1,23 @@
 const links = document.querySelectorAll('li a');
 
-links.forEach(link => {
+/* links.forEach(link => {
     link.addEventListener('click', () => {
         links.forEach(l => l.classList.remove('active')); // Reset all links
         link.classList.add('active'); // Highlight the clicked link
     });
 })
+ */
+for (let i = 0; i < links.length; i++) {
+    
+    links[i].addEventListener('click', function() {
+        for (let j = 0; j < links.length; j++) {
+            if (j !== i) {
+                links[j].classList.remove('active');
+            }
+        }
+        links[i].classList.add('active');
+
+        
+        
+    });
+}
